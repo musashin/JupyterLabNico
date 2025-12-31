67c10e3320bfc7d9f427ca93f165d6896d52bf2e
Nico <nulysse4@gmail.com>
2025-12-31 17:35:56 -0500

    docker container script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JupyterLabNico
 Nico Jupyter Lab notebook
+
+#tuto
+https://medium.com/technology-hits/starting-with-python-jupyter-in-docker-7d3ead8940cb
+
+#spin the docker container
+docker-compose -f ./jupyter-docker_compose.yml up
---
+++ build_docker_image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DOCKERPATH="docker"
+
+# forbiden character : "_", "-", " ", and uppercase
+NAME="dockerjupyter"
+VERSION='1.0'
+
+# continue docker image existance check
+DOCKERFILE=$DOCKERPATH//"Dockerfile"
+
+docker build --no-cache -t ${NAME}:${VERSION} -f ${DOCKERFILE} .
+
